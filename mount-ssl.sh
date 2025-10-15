77f6a411c86745302dd80322fe77ed963d5dfaa5
ThangNC <thangnc@comacpro.vn>
2025-10-15 21:25:22 +0700

    chore: update ssl

--- mount-ssl.sh
+++ mount-ssl.sh
@@ -7,4 +7,11 @@ mkdir -p "/opt/data/mailcow-certs"
 cp $CERT_SRC/fullchain.pem $CERT_DEST/cert.pem
 cp $CERT_SRC/privkey.pem   $CERT_DEST/key.pem
 cp $CERT_SRC/chain.pem     $CERT_DEST/chain.pem
+
+if [ ! -f "$CERT_DEST/dhparams.pem" ]; then
+    openssl dhparam -out $CERT_DEST/dhparams.pem 2048
+else
+    echo "✅ dhparams.pem đã tồn tại, bỏ qua."
+fi
+
 chmod 644 $CERT_DEST/*.pem
